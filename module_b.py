f7d3df3209919a0da0b3a845f579c2a4ffc0421b
Ina <10gongina@gmail.com>
2024-12-09 14:43:20 +0900

    히스토그램으로 변경
    그래프 종류를 히스토그램으로 변경

--- module_b.py
+++ module_b.py
@@ -5,29 +5,20 @@ def create_graph(df, subject):
     plt.rc('font', family='Malgun Gothic')
     plt.figure(figsize = (20,6))
 
-    # x축 값 :표준점수
-    x = list(df['표준점수'])
-
-    # 막대 너비
-    bar_width = 0.4
-
-    # 각 막대의 위치 계산
-    x_indexes_male = [i - bar_width / 2 for i in range(len(x))]
-    x_indexes_female = [i + bar_width / 2 for i in range(len(x))]
-
-    #남학생 그래프
-    plt.bar(x_indexes_male, df['남성 빈도수'], width=bar_width, label='남학생')
-
-    #여학생 그래프
-    plt.bar(x_indexes_female, df['여성 빈도수'], width=bar_width, label='여학생')
-
-    #표준점수 분포에 따라 x축 설정
-    plt.xticks(range(len(x)), x, rotation=90)
-
+    plt.hist( #히스토그램 생성
+        [df['표준점수'], df['표준점수']],  # 표준점수를 기준으로 두 데이터를 그룹화
+        bins=range(min(df['표준점수']), max(df['표준점수']) + 1, 5),  # 간격 5로 설정
+        weights=[df['남성 빈도수'], df['여성 빈도수']],
+        label=['남학생', '여학생'],
+        alpha=0.7,
+        edgecolor='black',
+        histtype='bar',
+    )
 
     plt.title(f'2024학년도 수능 {subject}과목 분포', fontsize=16)
     plt.xlabel('표준점수', fontsize=14)
     plt.ylabel('학생 수', fontsize=14)
+    plt.xticks(range(min(df['표준점수']), max(df['표준점수']) + 1, 5)) #x축 눈금을 5단위로 표시
     plt.legend()
     plt.tight_layout()
     plt.show()
